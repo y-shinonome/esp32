763618c935fb059456bfd4370feffc8ab7cdfae4
y-shinonome <y.shinonomail@gmail.com>
2022-03-20 01:43:09 +0900

    定数名、インデントの調整

--- motor.py
+++ motor.py
@@ -6,8 +6,8 @@ pwmRight = PWM(Pin(2), freq=200, duty_ns=1000000)
 
 SMALL_COEFFICIENT = 200000
 LARGE_COEFFICIENT = 400000
-MAX_DUTY_NS = 1750000
-MIN_DUTY_NS = 1000000
+MAX_DUTY_ns = 1750000
+MIN_DUTY_ns = 1000000
 
 def drive(drivingTime, gy271):
   stopTime = time() + int(drivingTime)
@@ -16,25 +16,23 @@ def drive(drivingTime, gy271):
     leftPulseWidth, rightPulseWidth = pulseWidth(gy271.directionalDifference())
     pwmLeft.duty_ns(leftPulseWidth)
     pwmRight.duty_ns(rightPulseWidth)
-
-    print(str(gy271.directionalDifference()) + ',' + str(pwmLeft.duty_ns()) + ',' + str(pwmRight.duty_ns()))
     sleep_ms(5)
 
-  pwmLeft.duty_ns(MIN_DUTY_NS)
-  pwmRight.duty_ns(MIN_DUTY_NS)
+  pwmLeft.duty_ns(MIN_DUTY_ns)
+  pwmRight.duty_ns(MIN_DUTY_ns)
 
-def pulseWidth(theta):
-    if theta >= 0:
-      leftPulseWidth = (MAX_DUTY_NS - (round(theta * SMALL_COEFFICIENT)))
-      rightPulseWidth = (MAX_DUTY_NS - (round(theta * LARGE_COEFFICIENT)))
-    elif theta < 0:
-      leftPulseWidth = (MAX_DUTY_NS + (round(theta * LARGE_COEFFICIENT)))
-      rightPulseWidth = (MAX_DUTY_NS + (round(theta * SMALL_COEFFICIENT)))
+def pulseWidth(radian):
+  if radian >= 0:
+    leftPulseWidth = (MAX_DUTY_ns - (round(radian * SMALL_COEFFICIENT)))
+    rightPulseWidth = (MAX_DUTY_ns - (round(radian * LARGE_COEFFICIENT)))
+  elif radian < 0:
+    leftPulseWidth = (MAX_DUTY_ns + (round(radian * LARGE_COEFFICIENT)))
+    rightPulseWidth = (MAX_DUTY_ns + (round(radian * SMALL_COEFFICIENT)))
 
-    if leftPulseWidth < MIN_DUTY_NS:
-      leftPulseWidth = MIN_DUTY_NS
-    
-    if rightPulseWidth < MIN_DUTY_NS:
-      rightPulseWidth = MIN_DUTY_NS
+  if leftPulseWidth < MIN_DUTY_ns:
+    leftPulseWidth = MIN_DUTY_ns
+  
+  if rightPulseWidth < MIN_DUTY_ns:
+    rightPulseWidth = MIN_DUTY_ns
 
-    return (leftPulseWidth, rightPulseWidth)
+  return (leftPulseWidth, rightPulseWidth)
